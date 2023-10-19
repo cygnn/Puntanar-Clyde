a28575d428088d329df9435388fcc2c0bdcc9038
Tapertii <113117498+cygnn@users.noreply.github.com>
2023-10-19 02:32:13 +0100

    Update and rename Main.java to PemdasCalculator.java

--- prelim/PemdasCalculator.java
+++ prelim/PemdasCalculator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class PemdasCalculator{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String expression;
